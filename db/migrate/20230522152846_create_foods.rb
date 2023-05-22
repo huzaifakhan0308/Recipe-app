35d0431b5fafab0302e028831786e14bb61d281c
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-22 19:19:28 +0300

    Setup database

--- db/migrate/20230522152846_create_foods.rb
+++ db/migrate/20230522152846_create_foods.rb
@@ -1,6 +1,11 @@
 class CreateFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :foods do |t|
+      t.string :name
+      t.string :measurement_unit
+      t.integer :quality, default: 0
+      t.decimal :price, default: 0
+      t.references :user, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20230522152922_create_recipe_foods.rb
+++ db/migrate/20230522152922_create_recipe_foods.rb
@@ -1,6 +1,9 @@
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
+      t.integer :quality, default: 0
+      t.references :recipe, null:false, foreign_key: true
+      t.references :food, null: false, foreign_key: true
 
       t.timestamps
     end
