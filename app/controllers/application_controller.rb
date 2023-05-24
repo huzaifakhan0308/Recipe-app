33884a81cd869bb531eea49de8252312255344bb
huzaifakhan0308 <huzaifa031252khan@gmail.com>
2023-05-24 15:07:58 +0500

    create navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, except: [:public_recipes]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
   def configure_permitted_parameters
-    attributes = %i[name]
+    attributes = %i[name role]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
