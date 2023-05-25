f36d0ada409a72d035f7afbf759cfa60a7680624
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-25 17:16:29 +0300

    Created food_recipe association

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,2 +1,6 @@
 class RecipeFood < ApplicationRecord
+  belongs_to :recipes, class_name: 'Recipe'
+  belongs_to :foods, class_name: 'Food'
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
+  validates :foods, uniqueness: { scope: :recipes_id }
 end
