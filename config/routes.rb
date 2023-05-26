3512deec50bc6487b1aebf26cd3ec2481a721983
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2023-05-26 10:55:58 +0300

    Modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :foods, only: [:index, :show, :new, :create, :destroy]
 
   resources :recipes, only: [:index, :show, :new, :create] do
     resources :recipe_foods, only: [:new, :create, :destroy]
   end
-  resources :foods, only: [:index, :new, :create, :destroy]
 
 
   authenticated :user do
